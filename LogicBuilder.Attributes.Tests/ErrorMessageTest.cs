fa4c72060bbd3c6eb2c5560d63b8f23f11f44833
Blaise Taylor <btaylor@bPolicySolutions.com>
2026-02-12 11:52:55 -0500

    AB#23 Creating the CI workflow. (#1)

--- LogicBuilder.Attributes.Tests/ErrorMessageTest.cs
+++ LogicBuilder.Attributes.Tests/ErrorMessageTest.cs
@@ -123,7 +123,7 @@ namespace LogicBuilder.Attributes.Tests
         {
             [ErrorMessage(TestConstants.ErrorMessageText)]
 #pragma warning disable CS0649 // Field is never assigned to, and will always have its default value
-            public string MyField = string.Empty;
+            public readonly string MyField = string.Empty;
 #pragma warning restore CS0649 // Field is never assigned to, and will always have its default value
 
             [ErrorMessage(TestConstants.ErrorMessageText)]
